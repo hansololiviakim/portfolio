2e7d01f82b0a1cd8b3157952eb53858caea086d2
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-02-09 23:39:25 +0900

    Design : 사용 이미지 추가

---
+++ public/images/icons/index.ts
@@ -0,0 +1,11 @@
+import company from './company.png'
+import github from './github.png'
+import mail from './mail.png'
+import notion from './notion.png'
+import school from './school.png'
+import star from './star.png'
+import tistory from './tistory.png'
+import web from './web.png'
+import write from './write.png'
+
+export { company, github, mail, notion, school, star, tistory, web, write }
---
+++ public/images/shapes/index.ts
@@ -0,0 +1,5 @@
+import empty from './empty.png'
+import fill from './fill.png'
+import sun from './sun.png'
+
+export { empty, fill, sun }
