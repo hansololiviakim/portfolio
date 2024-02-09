fbdb83fe7675685b637a620041de123bea6c72de
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-02-09 23:39:07 +0900

    Design : font, 기본 style 지정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
+import { Bai_Jamjuree } from '@next/font/google'
 import localFont from 'next/font/local'
 import '@/styles/globals.css'
 
+// * metadata: info
 export const metadata: Metadata = {
   title: '김한솔 | 포트폴리오',
   description: 'UX를 생각하는 프론트엔드 개발자 김한솔의 포트폴리오입니다.',
@@ -33,6 +35,7 @@ export const metadata: Metadata = {
   },
 }
 
+//  * metadata: viewport
 export const viewport = {
   width: 'device-width',
   initialScale: 1,
@@ -41,16 +44,22 @@ export const viewport = {
   interactiveWidget: 'overlays-content',
 }
 
+// * fonts
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
   display: 'swap',
   variable: '--font-pretendard',
 })
+const baiJamjuree = Bai_Jamjuree({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  variable: '--font-bai-jamjuree',
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" suppressHydrationWarning={true}>
-      <body className={`${pretendard.variable}`}>
+      <body className={`${pretendard.className} ${baiJamjuree.variable}`}>
         <main>{children}</main>
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,49 @@
 import type { Config } from 'tailwindcss'
+import { generateUtilities } from './src/utils/generateUtilities'
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gap: generateUtilities(100),
+      columnGap: generateUtilities(100),
+      rowGap: generateUtilities(100),
+      inset: generateUtilities(200),
+      width: generateUtilities(1100),
+      minWidth: generateUtilities(1100),
+      maxWidth: generateUtilities(1100),
+      height: generateUtilities(1100),
+      minHeight: generateUtilities(1100),
+      maxHeight: generateUtilities(1100),
+      margin: generateUtilities(200),
+      marginTop: generateUtilities(200),
+      marginRight: generateUtilities(200),
+      marginBottom: generateUtilities(200),
+      marginLeft: generateUtilities(200),
+      padding: generateUtilities(200),
+      paddingTop: generateUtilities(200),
+      paddingRight: generateUtilities(200),
+      paddingBottom: generateUtilities(200),
+      paddingLeft: generateUtilities(200),
+      border: generateUtilities(20),
+      borderWidth: generateUtilities(20),
+      borderRadius: generateUtilities(20),
+      fontSize: generateUtilities(70),
+      letterSpacing: generateUtilities(70),
+      lineHeight: generateUtilities(70),
+      fontFamily: {
+        pre: ['var(--font-pretendard)'],
+        bai: ['var(--font-bai-jamjuree)'],
+      },
+      colors: {
+        bl: '#161513',
+        re: '#E84D35',
+        gr: '#BDB8AD',
+        bg: '#F2F1EF',
       },
     },
   },
   plugins: [],
-}
+} satisfies Config
+
 export default config
