efebaf155d592bdcf100071d5ad54d7f8cef19b6
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-02-11 04:09:14 +0900

    Style : 포폴 이미지 썸네일 및 Header 태그 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { sun } from '../../public/images/shapes'
 
 const Header = () => {
   return (
-    <header className="header-width flex h-96 justify-between">
+    <nav className="header-width flex h-96 justify-between">
       <div className="flex-ctr-row cursor-default gap-12">
         <Image src={sun} width={32} height={32} alt="햇살 모양 아이콘" />
         <span className="title-5 font-bai font-bold">HANSOL</span>
@@ -15,7 +15,7 @@ const Header = () => {
         <li className="nav-button">Portfolio</li>
         <li className="nav-button">Contact</li>
       </ul>
-    </header>
+    </nav>
   )
 }
 
