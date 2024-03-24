077e0a6f27e5adb868dec6d56699f9a0f7393435
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2024-03-24 23:26:09 +0900

    Style : Experience 키트웍스 및 항해 detail 추가

--- src/components/common/ExpBox.tsx
+++ src/components/common/ExpBox.tsx
@@ -33,8 +33,8 @@ const ExpBox = () => {
               <ul>
                 {detail &&
                   detail.map((list, idx) => (
-                    <li key={idx} className="large-n text-[#E6E3DE]">
-                      {idx === 0 ? (
+                    <li key={idx} className={`large-n ${id === 'CAREER' ? 'text-[#E6E3DE]' : ''}`}>
+                      {id === 'CAREER' && idx === 0 ? (
                         <span>
                           • Vue2, Nuxt3를 사용한{' '}
                           <Link
@@ -45,6 +45,18 @@ const ExpBox = () => {
                             현대자동차 캐스퍼 웹 플랫폼
                           </Link>{' '}
                           운영
+                        </span>
+                      ) : id === 'EDUCATION' && idx === 1 ? (
+                        <span>
+                          • 함께 팀을 하고 싶은 사람이라는 동료 평가가 있었으며 수료 시{' '}
+                          <Link
+                            href="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbcWY8p%2FbtsmOMl8Z1j%2FkKk334mXYuHZ5MgELZwz81%2Fimg.png"
+                            target="_blank"
+                            className="font-medium underline hover:text-re"
+                          >
+                            팀워크 기여상
+                          </Link>{' '}
+                          수상
                         </span>
                       ) : (
                         <span>{list}</span>
